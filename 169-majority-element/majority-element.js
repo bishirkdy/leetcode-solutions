257e8879e6326fa94f92f6bce851fe4e8b6beb63
bishirkdy <143690400+bishirkdy@users.noreply.github.com>
2026-05-05 21:16:31 +0530

    Time: 4 ms (51.65%) | Memory: 58.5 MB (26.03%) - LeetSync

---
+++ 169-majority-element/majority-element.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+
+    let candidate = 0;
+    let count = 0;
+
+    for (let num of nums) {
+
+        if (count === 0) {
+            candidate = num;
+        }
+
+        if (candidate === num) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+
+    return candidate;
+};
